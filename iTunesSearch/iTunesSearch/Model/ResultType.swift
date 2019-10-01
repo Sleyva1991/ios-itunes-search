77ecb2bdf2cd4b2dc8dd0a2350bc3b486ecd701e
Steven Leyva <Sleyva1991@outlook.com>
2019-10-01 16:25:44 -0400

    Added SearchResult and ResultType files, added code to each of them

---
+++ iTunesSearch/iTunesSearch/Model/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Steven Leyva on 10/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "software"
+    case musicTrack = "musicTrack"
+    case movie = "movie"
+}
